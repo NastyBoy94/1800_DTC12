e5ea72419f4ac3b9dc4dbec1001b3f366121f7e7
George Rozitis <namacker.fire@gmail.com>
2021-11-05 17:21:53 -0700

    Added firebase and landing page

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA5yOcqB3Iu-HnMANfNQiNwV0vpntNGdd8",
+    authDomain: "dtc012-comp1800.firebaseapp.com",
+    projectId: "dtc012-comp1800",
+    storageBucket: "dtc012-comp1800.appspot.com",
+    messagingSenderId: "194965387452",
+    appId: "1:194965387452:web:f4e80ca20a0f8170f678b9",
+    measurementId: "G-W1J059450Z"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
