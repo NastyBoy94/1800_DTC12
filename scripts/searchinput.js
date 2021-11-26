a1c0d0be2aa5b0ede330db26a7386fb8d4934f7f
Cindy Liu <xian.liu@alum.utoronto.ca>
2021-11-26 01:57:57 -0800

    implement two function, getall and searchinput to search user input query

--- scripts/searchinput.js
+++ scripts/searchinput.js
@@ -1,20 +1,54 @@
-function myFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
+function getall() {
+    // contact items collection
+    db.collection("Items").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+
+}
+
+function searchinput(searchparam) {
+    // contact items collection
+    db.collection("Items").where("name", "==", searchparam).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+}
+
+
+// haev an event listent that looks up the search query submit, once that submit event is trigger, call a function
+// within that function, input to search 
+// when it seems someone click on searchquerysubmit it will call get all function
+// document.getElementById("searchQuerySubmit").addEventListener("click", getall)
+
+
+// function search() {
+//     // Declare variables
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById('searchQueryInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName('li');
   
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
-database.find
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < li.length; i++) {
+//       a = li[i].getElementsByTagName("a")[0];
+//       txtValue = a.textContent || a.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         li[i].style.display = "";
+//       } else {
+//         li[i].style.display = "none";
+//       }
+//     }
+//   }
+// database.find
+// // get all items
+// search - contain defined
